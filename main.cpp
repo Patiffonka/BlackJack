730aebcba8fe3aa78502db1438daadf127ba0f34
Sergey <Alamuth@mail.ru>
2020-06-04 20:33:08 +0700

    Lession 6.

--- main.cpp
+++ main.cpp
@@ -17,21 +17,59 @@ using namespace std;
 
 class Card {
 public:
-    enum cardRank {ACE=1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN=10, JACK=10, QUEEN=10, KING=10 };
+    enum cardRank {ACE=1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
     enum cardSuit {HEART, DIAMOND, CLUB, SPADE};
     Card (cardRank r, cardSuit s, bool f) : rank(r), suit (s), faceup(f) {}
     void Flip() {
         faceup = !faceup;
     }
     int GetValue() const {
-        return static_cast<int>(rank);
+        int rankInt;
+        if (rank == JACK || rank == QUEEN || rank == KING) rankInt = 10;
+        else rankInt = static_cast<int>(rank);
+        return rankInt;
     }
-    cardRank getRank(){return rank;}
+    cardRank getRank() const {return rank;}
+    cardSuit getSuit() const {return suit;}
+    bool faceUp() const {return faceup;}
+    friend ostream& operator<<(ostream& out, const Card &date);
+
 private:
     cardRank rank;
     cardSuit suit;
     bool faceup;
 };
+
+ostream& operator<<(ostream& out, const Card &date){
+    if (date.faceUp()) {
+    out << "|";
+    switch (date.rank) {
+        case Card::ACE: out << "A"; break;
+        case Card::TWO: out << "2"; break;
+        case Card::THREE: out << "3"; break;
+        case Card::FOUR: out << "4"; break;
+        case Card::FIVE: out << "5"; break;
+        case Card::SIX: out << "6"; break;
+        case Card::SEVEN: out << "7"; break;
+        case Card::EIGHT: out << "8"; break;
+        case Card::NINE: out << "9"; break;
+        case Card::TEN: out << "10"; break;
+        case Card::JACK: out << "J"; break;
+        case Card::QUEEN: out << "Q"; break;
+        case Card::KING: out << "K"; break;
+    }
+    out << " ";
+    switch (date.suit) {
+        case Card::HEART: out << "heart"; break;
+        case Card::DIAMOND: out << "diamond"; break;
+        case Card::CLUB: out << "club"; break;
+        case Card::SPADE: out << "spade"; break;
+    }
+    out << "| " << flush;
+    }
+    else cout << "|XXXX| " << flush;
+    return out;
+}
 
 class Hand {
 private:
@@ -47,15 +85,18 @@ public:
         int sum=0;
         for (unsigned int i = 0; i < hand.size(); ++i){
             if (hand.at(i)->getRank() == Card::ACE && ((sum+11)<21)) sum+=11;
-            else sum+=static_cast<int>(hand.at(i)->GetValue());
+            else sum+=hand.at(i)->GetValue();
         }
         return sum;
     }
     void print() {
         for (unsigned int i = 0; i < hand.size(); ++i)
-             cout << hand.at(i)->getRank() << endl;
+        {
+            cout << hand[i]->GetValue() << endl;
+        }
         cout << "sum: " << getValue() << endl;
     }
+    vector<Card*> getHand() const {return hand;}
 };
 
 class GenericPlayer: public Hand {
@@ -64,7 +105,7 @@ private:
 public:
     GenericPlayer(string n) : Hand(), name(n){}
     virtual ~GenericPlayer(){}
-    virtual bool isHitting(){return true;}                          //???
+    virtual bool isHitting() const {return true;}                          //???
     bool IsBoosted(){
         if (getValue()>21) return true;
         else return false;
@@ -72,20 +113,66 @@ public:
     void Bust(){
         cout << "Player " << name << ": Bust!" << endl;
     }
+    string getName() const {return name;}
+    friend ostream& operator<<(ostream& out, const GenericPlayer &player);
+};
+
+ostream& operator<<(ostream& out, const GenericPlayer &player){
+    out << "Player " << player.name << ": ";
+    for (unsigned int i=0; i < player.getHand().size(); ++i ){
+        out << *player.getHand()[i];
+    }
+    out << "\nSum: " << player.getValue() << endl;
+    return out;
+}
+
+class Player: public GenericPlayer {
+public:
+    Player(string n) : GenericPlayer(n){}
+//    virtual ~Player();
+    virtual bool isHitting() const {
+        cout << getName() << ", do you want a hit? (Y/N): ";
+        char answer;
+        cin >> answer;
+        return (answer == 'Y' || answer == 'y');
+    }
+    void win() const {
+        cout << "Player " << getName() << " win!" << endl;
+    }
+    void lose() const {
+        cout << "Player " << getName() << " lose!" << endl;
+    }
+    void push() const {
+        cout << "Player " << getName() << " push!" << endl;
+    }
+
+
+};
+
+class House: public GenericPlayer {
+public:
+    House(string name = "House"): GenericPlayer(name){}
+    virtual bool IsHitting() const {
+        return ((getValue()<16) ? true : false);
+    }
+    void FlipFirstCard(){
+        if (!getHand().empty()) getHand()[0]->Flip();
+        else cout << "hand is empty" << endl;
+    }
 };
 
 int main()
 {
-    GenericPlayer *player = new GenericPlayer("Vasyan");
-    Card *ACECLUB = new Card(Card::ACE, Card::CLUB, DOWN);
-    Card *TWODIAMOND = new Card(Card::TWO, Card::DIAMOND, DOWN);
-    Card *JACKSPADE = new Card(Card::JACK, Card::SPADE, DOWN);
-    Card *ACEDIAMOND = new Card(Card::ACE, Card::DIAMOND, DOWN);
-    player->add(ACECLUB);
-    player->add(TWODIAMOND);
-    player->add(JACKSPADE);
-    player->add(ACEDIAMOND);
-    player->print();
-    if (player->IsBoosted()) player->Bust();
-
+    Card *KINGDIAMOND = new Card(Card::KING, Card::DIAMOND, UP);
+    Card *ACEHEART = new Card(Card::ACE, Card::HEART, UP);
+    Card *TWOSPADE = new Card(Card::TWO, Card::SPADE, UP);
+    Card *EIGHTCLUB = new Card(Card::EIGHT, Card::CLUB, UP);
+    cout << *TWOSPADE << endl;
+    GenericPlayer *VASYAN = new GenericPlayer("Vasyan");
+    TWOSPADE->Flip();
+    VASYAN->add(KINGDIAMOND);
+    VASYAN->add(ACEHEART);
+    VASYAN->add(TWOSPADE);
+    VASYAN->add(EIGHTCLUB);
+    cout << *VASYAN << endl;
 }
